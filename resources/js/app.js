d11927870bd1fc65ec0aafff2d62e02d74e4155f
yumauma20 <71yuma10@gmail.com>
2020-03-02 20:02:27 +0900

    タスク詳細コンポーネント実装

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import VueRouter from 'vue-router';
 import HeaderComponent from "./components/HeaderComponent";
 import TaskListComponent from "./components/TaskListComponent";
+import TaskShowComponent from "./components/TaskShowComponent";
 
 require('./bootstrap');
 
@@ -15,6 +16,12 @@ const router = new VueRouter({
             path: '/tasks',
             name: 'task.list',
             component: TaskListComponent
+        },
+        {
+            path: '/tasks/:taskId',
+            name: 'task.show',
+            component: TaskShowComponent,
+            props: true
         },
     ]
 });
